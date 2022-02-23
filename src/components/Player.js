4268056574d2fae3adb8bbae349e16345784c418
Reinoptland <reinoptland@gmail.com>
2022-02-23 10:47:40 +0100

    Slight cleanup

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,26 +3,27 @@ import { useState } from "react";
 export default function Player() {
   const [player, setPlayer] = useState({ name: "Rein", score: 0 });
   const [editMode, setEditMode] = useState(true);
+
+  function changeScore(number) {
+    setPlayer({ ...player, score: player.score + number });
+  }
+
+  function changeName(input) {
+    setPlayer({ ...player, name: input });
+  }
+
   return (
     <div>
       <h2>{player.name}</h2>
       <h3>{player.score}</h3>
       <div>
-        <button
-          onClick={(e) => setPlayer({ ...player, score: player.score - 1 })}
-        >
-          -
-        </button>
-        <button
-          onClick={(e) => setPlayer({ ...player, score: player.score + 1 })}
-        >
-          +
-        </button>
+        <button onClick={() => changeScore(-1)}>-</button>
+        <button onClick={() => changeScore(1)}>+</button>
       </div>
       {editMode && (
         <input
           value={player.name}
-          onChange={(e) => setPlayer({ ...player, name: e.target.value })}
+          onChange={(e) => changeName(e.target.value)}
         />
       )}
       {editMode ? (
